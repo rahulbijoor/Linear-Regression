edd02cf000d441980f84e6edd55662d2093ea900
rahulbijoor <rahulbijoor@gmail.com>
2023-12-17 15:58:44 -0800

    Extract individual columns

--- superml.py
+++ superml.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # coding: utf-8
 
+#Linear Regression is a supervised machine learning techinique used to predict a numeric value from one or more input features. 
 
+# This program uses linear regression to predict the scores of students based on the number of hours they studied.
 
-import numpy as np
+# first we import the necessary libraries
+import numpy as np 
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
@@ -13,11 +16,12 @@ from sklearn.linear_model import LinearRegression
 
 
 
-
+# we get the data from file student_scores.csv
 df = pd.read_csv('student_scores.csv')
 print(df.head(10))
 
-
+X=df['Hours']  # X is the value to be predicted
+Y=df['Scores'] # Y is the value based on which the prediction is made
 
 
 
